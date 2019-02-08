1af55efa4e6a9a82da34920f35bf4ef8f2bf2eda
BBadoo <bbader0806@gmail.com>
2019-02-08 10:50:48 -0500

    working

--- javascript/init.js
+++ javascript/init.js
@@ -4,6 +4,7 @@ var mdi = require=("@mdi/font")
 $(document).ready(function(){
   $('.fixed-action-btn').floatingActionButton();
   var instance = M.FloatingActionButton.getInstance(elem);
+  
 
   (function($){
     $(function(){
