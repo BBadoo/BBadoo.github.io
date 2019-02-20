f8e874fdf6b07cdc1151cb38bb2dc0aabc74f1a1
BBadoo <bbader0806@gmail.com>
2019-02-20 10:42:13 -0500

    sidenavs

--- javascript/init.js
+++ javascript/init.js
@@ -3,11 +3,12 @@ var mdi = require=("@mdi/font")
 
 $(document).ready(function(){
   $('.fixed-action-btn').floatingActionButton();
+  $('.sidenav').sidenav();
   // var instance = M.FloatingActionButton.getInstance(elem);
 
   (function($){
     $(function(){
   
-      $('.sidenav').sidenav();
+     
       $('.parallax').parallax();
 })})})
